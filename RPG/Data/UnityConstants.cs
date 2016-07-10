2c617a493eefef5fbb3cce8c592ed8be6cd7489f
Hifior <625673575@qq.com>
2016-07-10 14:38:21 +0800

    7.10

---
+++ RPG/Data/UnityConstants.cs
@@ -0,0 +1,210 @@
+// This file is auto-generated. Modifications are not saved.
+
+namespace UnityConstants
+{
+    public static class Tags
+    {
+        /// <summary>
+        /// Name of tag 'Untagged'.
+        /// </summary>
+        public const string Untagged = "Untagged";
+        /// <summary>
+        /// Name of tag 'Respawn'.
+        /// </summary>
+        public const string Respawn = "Respawn";
+        /// <summary>
+        /// Name of tag 'Finish'.
+        /// </summary>
+        public const string Finish = "Finish";
+        /// <summary>
+        /// Name of tag 'EditorOnly'.
+        /// </summary>
+        public const string EditorOnly = "EditorOnly";
+        /// <summary>
+        /// Name of tag 'MainCamera'.
+        /// </summary>
+        public const string MainCamera = "MainCamera";
+        /// <summary>
+        /// Name of tag 'Player'.
+        /// </summary>
+        public const string Player = "Player";
+        /// <summary>
+        /// Name of tag 'GameController'.
+        /// </summary>
+        public const string GameController = "GameController";
+    }
+
+    public static class SortingLayers
+    {
+        /// <summary>
+        /// ID of sorting layer 'Default'.
+        /// </summary>
+        public const int Default = 0;
+    }
+
+    public static class Layers
+    {
+        /// <summary>
+        /// Index of layer 'Default'.
+        /// </summary>
+        public const int Default = 0;
+        /// <summary>
+        /// Index of layer 'TransparentFX'.
+        /// </summary>
+        public const int TransparentFX = 1;
+        /// <summary>
+        /// Index of layer 'Ignore Raycast'.
+        /// </summary>
+        public const int Ignore_Raycast = 2;
+        /// <summary>
+        /// Index of layer 'Water'.
+        /// </summary>
+        public const int Water = 4;
+        /// <summary>
+        /// Index of layer 'UI'.
+        /// </summary>
+        public const int UI = 5;
+        /// <summary>
+        /// Index of layer 'Terrain'.
+        /// </summary>
+        public const int Terrain = 8;
+
+        /// <summary>
+        /// Bitmask of layer 'Default'.
+        /// </summary>
+        public const int DefaultMask = 1 << 0;
+        /// <summary>
+        /// Bitmask of layer 'TransparentFX'.
+        /// </summary>
+        public const int TransparentFXMask = 1 << 1;
+        /// <summary>
+        /// Bitmask of layer 'Ignore Raycast'.
+        /// </summary>
+        public const int Ignore_RaycastMask = 1 << 2;
+        /// <summary>
+        /// Bitmask of layer 'Water'.
+        /// </summary>
+        public const int WaterMask = 1 << 4;
+        /// <summary>
+        /// Bitmask of layer 'UI'.
+        /// </summary>
+        public const int UIMask = 1 << 5;
+        /// <summary>
+        /// Bitmask of layer 'Terrain'.
+        /// </summary>
+        public const int TerrainMask = 1 << 8;
+    }
+
+    public static class Scenes
+    {
+        /// <summary>
+        /// ID of scene 'Logo'.
+        /// </summary>
+        public const int Logo = 0;
+        /// <summary>
+        /// ID of scene 'GameStartMenu'.
+        /// </summary>
+        public const int GameStartMenu = 1;
+        /// <summary>
+        /// ID of scene 'LoadingScene'.
+        /// </summary>
+        public const int LoadingScene = 2;
+        /// <summary>
+        /// ID of scene 'StartScene'.
+        /// </summary>
+        public const int StartScene = 3;
+        /// <summary>
+        /// ID of scene 'ChapterEndSave'.
+        /// </summary>
+        public const int ChapterEndSave = 4;
+        /// <summary>
+        /// ID of scene 'BattleTemplate'.
+        /// </summary>
+        public const int BattleTemplate = 5;
+    }
+
+    public static class Axes
+    {
+        /// <summary>
+        /// Input axis 'Horizontal'.
+        /// </summary>
+        public const string Horizontal = "Horizontal";
+        /// <summary>
+        /// Input axis 'Vertical'.
+        /// </summary>
+        public const string Vertical = "Vertical";
+        /// <summary>
+        /// Input axis 'Yes'.
+        /// </summary>
+        public const string Yes = "Yes";
+        /// <summary>
+        /// Input axis 'No'.
+        /// </summary>
+        public const string No = "No";
+        /// <summary>
+        /// Input axis 'Start'.
+        /// </summary>
+        public const string Start = "Start";
+        /// <summary>
+        /// Input axis 'L1'.
+        /// </summary>
+        public const string L1 = "L1";
+        /// <summary>
+        /// Input axis 'R1'.
+        /// </summary>
+        public const string R1 = "R1";
+        /// <summary>
+        /// Input axis 'A'.
+        /// </summary>
+        public const string A = "A";
+        /// <summary>
+        /// Input axis 'B'.
+        /// </summary>
+        public const string B = "B";
+        /// <summary>
+        /// Input axis 'X'.
+        /// </summary>
+        public const string X = "X";
+        /// <summary>
+        /// Input axis 'Y'.
+        /// </summary>
+        public const string Y = "Y";
+        /// <summary>
+        /// Input axis 'Submit'.
+        /// </summary>
+        public const string Submit = "Submit";
+        /// <summary>
+        /// Input axis 'Cancel'.
+        /// </summary>
+        public const string Cancel = "Cancel";
+        /// <summary>
+        /// Input axis 'Mouse ScrollWheel'.
+        /// </summary>
+        public const string Mouse_ScrollWheel = "Mouse ScrollWheel";
+        /// <summary>
+        /// Input axis 'ZoomIn'.
+        /// </summary>
+        public const string ZoomIn = "ZoomIn";
+        /// <summary>
+        /// Input axis 'ZoomOut'.
+        /// </summary>
+        public const string ZoomOut = "ZoomOut";
+        /// <summary>
+        /// Input axis 'Up'.
+        /// </summary>
+        public const string Up = "Up";
+        /// <summary>
+        /// Input axis 'Down'.
+        /// </summary>
+        public const string Down = "Down";
+        /// <summary>
+        /// Input axis 'Left'.
+        /// </summary>
+        public const string Left = "Left";
+        /// <summary>
+        /// Input axis 'Right'.
+        /// </summary>
+        public const string Right = "Right";
+    }
+}
+
---
+++ Util/AnimParamsAttribute.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+/*
+Use the AnimParams attribute on a string property/variable and the inspector will show a popup that lists all the parameter names available in the given Animator.
+Pass the name of the Animator property as a string to the AnimParams attribute.
+
+public class ExampleView : MonoBehaviour
+{
+    public Animator anim;
+
+    // These are strings that we'll use to fire parameters in code
+    // pass the name of the Animator variable/property as a string
+    [AnimParams("anim")]
+    public string paramIdleBool;
+
+    [AnimParams("anim")]
+    public string paramAttackTrigger;
+
+    void Start()
+    {
+        // Use the property which will be filled with one of the
+        // parameters names available in the animator in the inspector
+        anim.SetBool(paramIdleBool, true);
+    }
+}
+*/
+namespace Utils
+{
+    public class AnimParamsAttribute : PropertyAttribute
+    {
+        public string animatorProp;
+
+        public AnimParamsAttribute(string animatorPropName)
+        {
+            animatorProp = animatorPropName;
+        }
+    }
+}
